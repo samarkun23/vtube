423b0f0f4da4bca366e6e9770590c0fa8629e6ee
samarkun <samarkun4@gmail.com>
2025-03-16 22:18:06 -0700

    adding uploading fields

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
 import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
 
 //if maine url mai /register lagaya to ye register method call ho jayega
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    //ye uplaod hai middleware 
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1 
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 
 export default router
