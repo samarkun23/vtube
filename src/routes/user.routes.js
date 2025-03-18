8b7b9c45a23d667cbb282f1978ab94374a2083a0
samarkun <samarkun4@gmail.com>
2025-03-18 02:51:45 -0700

    update routes for login

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controller/user.controller.js";
+import { loginUser, registerUser } from "../controller/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
@@ -21,5 +21,6 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post(loginUser)
 
 export default router
